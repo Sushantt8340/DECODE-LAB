19f14e13017cd98585e3ddfece2380f0cd650d1a
sushant kumar <sushant8809045@gmail.com.com>
2026-06-26 16:27:50 +0530

    chore: remove PROJECT02 folder and files

--- PROJECT02/travel-api/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitContact } = require('../controllers/contactController');
-const { validateContact } = require('../middleware/validator');
-
-// POST /api/contact - Submit a contact message (with validation)
-router.post('/', validateContact, submitContact);
-
-module.exports = router;
